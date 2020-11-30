a8ff1a3e30239fad228cbafe80e60dd75e19adc3
unknown <jjbi@uci.edu>
2020-11-29 19:23:16 -0800

    Shared attributes in service and refactors

--- client/src/app/services/sentiment.service.ts
+++ client/src/app/services/sentiment.service.ts
@@ -7,35 +7,31 @@ import { SentimentData } from '../data/sentiment-data';
 })
 export class SentimentService {
     expressBaseUrl:string = 'http://localhost:8888';
+    course:string = null;
+    date:Date = null;
 
-  constructor(private http:HttpClient) { }
+    constructor(private http:HttpClient) { }
 
-  private sendRequestToExpress(endpoint:string):Promise<any> {
-    //TODO: use the injected http Service to make a get request to the Express endpoint and return the response.
-    //the http service works similarly to fetch(). It may be useful to call .toPromise() on any responses.
-    //update the return to instead return a Promise with the data from the Express server
-    return Promise.resolve(this.http.get(this.expressBaseUrl+endpoint).toPromise());
-  }
+    // returns a promise to resolve an http get request
+    private sendRequestToExpress(endpoint:string):Promise<any> {
+        return Promise.resolve(this.http.get(this.expressBaseUrl+endpoint).toPromise());
+    }
 
-  // specify the course
-  getMessageData(courseName:string, timestamp:Date):Promise<SentimentData[]> {
-    console.log(window.location.pathname);
-    // timestamp should be a date object, returned messages will be of dates < timestamp
-      // replace timestamp period with comma
-    return this.sendRequestToExpress('/messages/' + encodeURIComponent(courseName) + '/' + encodeURIComponent(timestamp.toISOString().replace('.',',') )).then( (data) => {
-        console.log(data);
-        return Object.values(data).map ( (message) => {
-            return new SentimentData(message);
+    // gets array of sentiment data to send to graph, applies filters on course and timestamp
+    getMessageData(courseName:string, timestamp:Date):Promise<SentimentData[]> {
+        // timestamp should be a date object, processing is done here
+        return this.sendRequestToExpress('/messages/' + encodeURIComponent(courseName) + '/' + encodeURIComponent(timestamp.toISOString().replace('.',',') )).then( (data) => {
+            console.log(data);
+            return Object.values(data).map ( (message) => {
+                return new SentimentData(message);
+            });
         });
-    });
-  }
+    }
 
+    // gets an array of courses
     getCourses():Promise<String[]> {
         return this.sendRequestToExpress('/courses').then((data) => {
             console.log(data);
-            // return data.map( (course) => {
-            //     return 
-            // })
             return data;
         });
     }
--- webserver/routes/index.js
+++ webserver/routes/index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-//Fetch doesn't exist on server-side JavaScript, so we impoort a package which implements the functionality.
-var fetch = require('node-fetch');
-var fs = require('fs');
-var client_uri = 'http://localhost:4200';
+
+// firebase database setup with view only private key credentials
 var serviceAccount = require("../gcloud_private.json");
 const admin=require('firebase-admin');
 var databaseURL = "https://sentiment-analyzer-295302.firebaseio.com/";
@@ -14,13 +12,16 @@ admin.initializeApp({
 var db = admin.database();
 var ref = db.ref("/");
 
+
+// rate limiting: 1000 requests per day
 const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
-	windowMs: 24 * 60 * 60 * 1000, // 24 hrs
-	max: 1000 // limit each IP to 1000 requests per windowMs
+	windowMs: 24 * 60 * 60 * 1000,
+	max: 1000
 });
 router.use(limiter);
 
+// courses endpoint that returns list of courses
 router.get('/courses', (req, res, next) => {
 	ref.child('courses').once('value').then((dataSnapshot) => {
 		return dataSnapshot.val();
@@ -31,13 +32,11 @@ router.get('/courses', (req, res, next) => {
 	});
 });
 
+// messages endpoint that returns an object of sentiment data filtered by course and timestamp
 router.get('/messages/:course/:timestamp', (req, res, next) => {
-	// course should be a string
 	var course = req.params.course;
 	var timestamp = req.params.timestamp;
 	ref.child(course).orderByKey().startAt(timestamp).once('value').then((dataSnapshot) => {
-		// handle read data.
-		// console.log(dataSnapshot.val);
 		return dataSnapshot.val();
 	}).then(json => {
 		res.json(json);
